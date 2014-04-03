6b445705938c68e6cbffeb1bc047dbce8d98dde2
Carlos Escriche Izquierdo <escrichov@gmail.com>
2014-04-03 13:48:25 +0200

    Arreglado módulo de google_map_kml

--- openerp/addons_extra/google_map_kml/google_map_partner.py
+++ openerp/addons_extra/google_map_kml/google_map_partner.py
@@ -1,21 +1,83 @@
 from openerp.osv import osv, fields
+from openerp import tools
 
+try:
+    import simplejson as json
+except ImportError:
+    import json
+import urllib
 
 class google_map_partner(osv.osv):
 
     _inherit = "res.partner"
+    _name = "res.partner"
     
     _columns = {
         'googlemap_visited': fields.boolean('Visited'),
+        'partner_latitude': fields.float('Geo Latitude'),
+        'partner_longitude': fields.float('Geo Longitude'),
+        'date_localization': fields.date('Geo Localization Date'),
     }
     
     _defaults = {
         'googlemap_visited': False   
     }
-#     
-#     def write(self, cr, uid, ids, vals, context=None):
-#         self.geo_localize(cr, uid, ids)
-#         return super(google_map_partner, self).write(self, cr, uid, ids, vals, context=context)
+    
+    def geo_find(self, cr, uid, addr, context=None):
+        key = self.pool.get('ir.config_parameter').get_param(cr, uid, "geocode_key", context)
+        if key:
+            url = 'https://maps.googleapis.com/maps/api/geocode/json?sensor=false&key='+ key +'&address='
+        else:
+            url = 'https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address='
+        url += urllib.quote(addr.encode('utf8'))
+    
+        try:
+            result = json.load(urllib.urlopen(url))
+        except Exception, e:
+            raise osv.except_osv('Network error',
+                                 'Cannot contact geolocation servers. Please make sure that your internet connection is up and running (%s).' % e)
+        if result['status'] != 'OK':
+            return None
+    
+        try:
+            geo = result['results'][0]['geometry']['location']
+            return float(geo['lat']), float(geo['lng'])
+        except (KeyError, ValueError):
+            return None
+
+    def geo_query_address(self, street=None, zip=None, city=None, state=None, country=None):
+        if country and ',' in country and (country.endswith(' of') or country.endswith(' of the')):
+            # put country qualifier in front, otherwise GMap gives wrong results,
+            # e.g. 'Congo, Democratic Republic of the' => 'Democratic Republic of the Congo'
+            country = '{1} {0}'.format(*country.split(',', 1))
+        return tools.ustr(', '.join(filter(None, [street,
+                                                  ("%s %s" % (zip or '', city or '')).strip(),
+                                                  state,
+                                                  country])))
+ 
+    def write(self, cr, uid, ids, vals, context=None):
+         write_return = super(google_map_partner, self).write(cr, uid, ids, vals, context=context)
+         self.geo_localize(cr, uid, ids)
+         return write_return
+    
+    def geo_localize(self, cr, uid, ids, context=None):
+        # Don't pass context to browse()! We need country names in english below
+        for partner in self.browse(cr, uid, ids):
+            if not partner:
+                continue
+            result = self.geo_find(cr, uid, self.geo_query_address(street=partner.street,
+                                                zip=partner.zip,
+                                                city=partner.city,
+                                                state=partner.state_id.name,
+                                                country=partner.country_id.name), context=context)
+            if result:
+                super(google_map_partner, self).write(cr, uid, [partner.id], {
+                    'partner_latitude': result[0],
+                    'partner_longitude': result[1],
+                    'date_localization': fields.date.context_today(self, cr, uid, context=context)
+                }, context=context)
+        return True
+    
     
     def create(self, cr, uid, vals, context={}):
         partner_id = super(google_map_partner, self).create(cr, uid, vals, context)
@@ -37,17 +99,28 @@ class google_map_partner(osv.osv):
         }
         
     def open_map_partners_wizard(self, cr, uid, ids, context=None):
-        partner = self.pool.get('res.partner')
+        #partner = self.pool.get('res.partner')
+        
+        # view_ref = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'google_map_kml', 'view_partner_form_map_wizard_client')
+        #view_id = view_ref and view_ref[1] or False
+        
+        return {
+            'name': 'Google Maps Wizard',
+            'type': 'ir.actions.client',
+            'tag': 'location_map.partners',
+        }
+        
+    def open_map_partners_wizard_new(self, cr, uid, ids, context=None):
         
         view_ref = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'google_map_kml', 'view_partner_form_map_wizard')
         view_id = view_ref and view_ref[1] or False
         
         return {
-            'name': 'Google Maps Wizard',
-            'type': 'ir.actions.act_window',
-            'res_model': 'res.partner',
-            'view_id': view_id,
-            'target': 'new',
-            'view_mode': 'form'
+             'name': 'Google Maps Wizard',
+             'type': 'ir.actions.act_window',
+             'res_model': 'res.partner',
+             'view_id': view_id,
+             'target': 'new',
+             'view_mode': 'form'
         }
 
--- openerp/addons_extra/google_map_kml/static/src/js/location_map_partners.js
+++ openerp/addons_extra/google_map_kml/static/src/js/location_map_partners.js
@@ -3,22 +3,27 @@ function map_loaded() {
 	
 }
 
-openerp.crm_partner_map = function (instance)
+openerp.google_map_kml = function (instance)
 {   
+	var _t = instance.web._t,
+    _lt = instance.web._lt;
+	var QWeb = instance.web.qweb;
+	
+	instance.google_map_kml = {};
+	
+	
 
-	instance.web.form.widgets.add('location_map_partners', 'instance.crm_partner_map.Map');
-
-	instance.crm_partner_map.Map = instance.web.form.AbstractField.extend({
+	instance.google_map_kml.MapPartner = instance.web.form.AbstractField.extend({
 		map: null, // google.maps.Map instance
 		markers: [], // partner's location marker
-		template: 'location_map_partners',
+		template: 'location_map.partners_new_window',
 		create_markers: function() {
 			if(this.map != null) {
 				var Partner = new instance.web.Model('res.partner');
 				Partner.query(['partner_latitude', 'partner_longitude', 'name', 'googlemap_visited']).all().done($.proxy(function(partners) {
 					// create markers
 					var bounds = new google.maps.LatLngBounds();
-					
+					var map = this.map;
 					for(var i=0; i < partners.length; i++){
 						var lat = partners[i].partner_latitude;
 						var lng = partners[i].partner_longitude;
@@ -61,13 +66,88 @@ openerp.crm_partner_map = function (instance)
 			}
 			
 		},
-		init: function(parent, options) {
+		init: function(field_manager, node) {
 			//location_map_widget = this; // needed for workaround below
-			// load google's api		
-			this._super(parent, options);
+			// load google's api
+			console.log("entra");
+			this._super(field_manager, node);
+			this.name = name;
 		},
-		
-		start: function () {
+		start: function(){
+			// initialize the widget with new map
+			if(typeof google !== "undefined") { // clear offline usage errors....
+				    this.map = new google.maps.Map(document.getElementById("location_map_partners2"), 
+									{zoom: 6, mapTypeId: google.maps.MapTypeId.ROADMAP});				
+			} else {
+				this.$el.text("Couldn't load Goole Map API. Please check internet connection and reload the page.");
+			}
+			
+			// have to refresh the value
+			this.create_markers();
+		},
+	});
+	
+	instance.google_map_kml.OpenMap = instance.web.Widget.extend({
+		map: null, // google.maps.Map instance
+		markers: [], // partner's location marker
+		template: 'location_map.partners',
+		create_markers: function() {
+			if(this.map != null) {
+				var Partner = new instance.web.Model('res.partner');
+				Partner.query(['partner_latitude', 'partner_longitude', 'name', 'googlemap_visited']).all().done($.proxy(function(partners) {
+					// create markers
+					var bounds = new google.maps.LatLngBounds();
+					var map = this.map;
+					for(var i=0; i < partners.length; i++){
+						var lat = partners[i].partner_latitude;
+						var lng = partners[i].partner_longitude;
+						var pos = new google.maps.LatLng(lat, lng);
+						if(lat != 0 && lng != 0 && partners[i].googlemap_visited === true){
+							bounds.extend(pos);
+						}
+						if(this.map) {
+							if(lat != 0.0 & lng != 0.0 && partners[i].googlemap_visited === true) {
+								
+								if(typeof this.markers[i] !== "undefined" && this.markers[i] != null) {
+									this.markers[i].setMap(null);
+									delete this.markers;									
+								}
+								
+								this.map.panTo(new google.maps.LatLng(lat, lng));
+								
+								var marker = new google.maps.Marker({
+									position : new google.maps.LatLng(lat, lng),
+									title: partners[i].name,
+									map : this.map,
+									draggable : false,
+									raiseOnDrag : true});
+								
+								this.markers[i] = marker;
+								
+					            var infoWindow = new google.maps.InfoWindow({
+					                content: partners[i].name
+					            });
+					            
+					            infoWindow.open(this.map, this.markers[i]);
+					            google.maps.event.addListener(this.markers[i], 'click', function () {
+					                infoWindow.open(map, marker);
+					            });
+							}
+						}
+					}
+					this.map.fitBounds(bounds);
+				}, this));
+			}
+			
+		},
+		init: function(parent, name) {
+			//location_map_widget = this; // needed for workaround below
+			// load google's api
+			
+			this._super(parent);
+			this.name = name;
+		},
+		start: function(){
 			// initialize the widget with new map
 			if(typeof google !== "undefined") { // clear offline usage errors....
 				    this.map = new google.maps.Map(document.getElementById("location_map_partners"), 
@@ -78,10 +158,10 @@ openerp.crm_partner_map = function (instance)
 			
 			// have to refresh the value
 			this.create_markers();
-			return this._super();
 		},
-
 	});
-
+	
+	instance.web.client_actions.add('location_map.partners', 'instance.google_map_kml.OpenMap');
+	instance.web.form.widgets.add('google_map_partner', 'instance.google_map_kml.MapPartner');
 }
 
