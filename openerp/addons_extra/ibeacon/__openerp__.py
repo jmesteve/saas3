4f1109b8217aff822bcc5569536a64d86f954d13
Carlos Escriche Izquierdo <escrichov@gmail.com>
2014-05-17 10:34:22 +0200

    Arreglado problema al instalar el modulo de iBeacons

--- openerp/addons_extra/ibeacon/__openerp__.py
+++ openerp/addons_extra/ibeacon/__openerp__.py
@@ -23,7 +23,7 @@
 ##############################################################################
 {
     'name': 'ibeacon',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'ErpAndCloud',
     'category': 'iBeacon',    
     'description': """
