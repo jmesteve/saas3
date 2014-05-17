9160ced5eba2542b9f77b3ea2f28c103fb095633
Carlos Escriche Izquierdo <escrichov@gmail.com>
2014-05-17 15:23:40 +0200

    Modificados comandos de update y uninstall

--- openerpcommand/uninstall.py
+++ openerpcommand/uninstall.py
@@ -22,25 +22,41 @@ def create_database(database_name):
         cr.close()
 
 def run(args):
-    assert args.database
+    #assert args.database
     assert args.module
 
     import openerp
 
     config = openerp.tools.config
-    config['log_handler'] = [':CRITICAL']
-    if args.addons:
-        args.addons = args.addons.split(':')
+
+    default_config_file = '../openerp-server.conf'
+    if args.config == None:
+        config_file = default_config_file
     else:
-        args.addons = []
-    config['addons_path'] = ','.join(args.addons)
+        config_file = args.config
+    
+    config.parse_config(['-c', config_file])
+
+    config['log_handler'] = [':CRITICAL']
+#    if args.addons:
+#        args.addons = args.addons.split(':')
+#    else:
+#        args.addons = []
+#    config['addons_path'] = ','.join(args.addons)
     openerp.netsvc.init_logger()
 
     # Install the import hook, to import openerp.addons.<module>.
     openerp.modules.module.initialize_sys_path()
 
-    registry = openerp.modules.registry.RegistryManager.get(
-        args.database, update_module=False)
+    if args.database != None:
+        database = args.database
+    elif config['db_name'] != None:
+        database = config['db_name']
+    
+    if database == None:
+		return -1;
+
+    registry = openerp.modules.registry.RegistryManager.get(database, update_module=False)
 
     ir_module_module = registry.get('ir.module.module')
     cr = registry.db.cursor() # TODO context manager
@@ -49,17 +65,18 @@ def run(args):
         if len(ids) == len(args.module):
             ir_module_module.button_immediate_uninstall(cr, openerp.SUPERUSER_ID, ids, {})
         else:
-            print "At least one module not found (database `%s`)." % (args.database,)
+            print "At least one module not found (database `%s`)." % (database,)
     finally:
         cr.close()
 
 def add_parser(subparsers):
     parser = subparsers.add_parser('uninstall',
         description='Uninstall some modules from an OpenERP database.')
-    parser.add_argument('-d', '--database', metavar='DATABASE',
-        **common.required_or_default('DATABASE', 'the database to modify'))
-    common.add_addons_argument(parser)
-    parser.add_argument('--module', metavar='MODULE', action='append',
+    parser.add_argument('-d', '--database', metavar='DATABASE', required=False)
+    #common.add_addons_argument(parser)
+    parser.add_argument('-c', '--config', metavar='CONFIG FILE', required=False,
+        help='the config file')
+    parser.add_argument('-m','--module', metavar='MODULE', action='append',
         help='specify a module to uninstall'
         ' (this option can be repeated)')
 
--- openerpcommand/update.py
+++ openerpcommand/update.py
@@ -3,17 +3,48 @@ Update an existing OpenERP database.
 """
 
 def run(args):
-    assert args.database
+    #assert args.database
     import openerp
     config = openerp.tools.config
-    config['update']['all'] = 1
+    
+    
+    default_config_file = '../openerp-server.conf'
+    if args.config == None:
+        config_file = default_config_file
+    else:
+        config_file = args.config
+    config.parse_config(['-c', config_file])
+    
+    if args.database != None:
+        database = args.database
+    elif config['db_name'] != None:
+        database = config['db_name']
+    
+    openerp.netsvc.init_logger()
+    
+    if database == None:
+        print """ No database name"""
+        return -1
+    
+    if args.all != None:
+        config['update']['all'] = 1
+    else:
+        modules = args.modules.split(',')
+        for module in modules:
+            config['update'][module] = 1
     openerp.modules.registry.RegistryManager.get(
-        args.database, update_module=True)
+        database, update_module=True)
+
 
 def add_parser(subparsers):
     parser = subparsers.add_parser('update',
         description='Update an existing OpenERP database.')
-    parser.add_argument('-d', '--database', metavar='DATABASE', required=True,
+    parser.add_argument('-c', '--config', metavar='CONFIG FILE', required=False,
+        help='the config file')
+    parser.add_argument('-m', '--modules', metavar='MODULES', required=True,
+        help='the addons to update')
+    parser.add_argument('-d', '--database', metavar='DATABASE', required=False,
         help='the database to update')
+    parser.add_argument('-a', '--all', metavar='ALL', required=False, help='Update all addons')
 
     parser.set_defaults(run=run)
