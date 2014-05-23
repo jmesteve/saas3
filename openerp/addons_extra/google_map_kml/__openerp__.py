18400f5d3c6b02281384420b5e67d56fdba57aa3
Carlos Escriche Izquierdo <escrichov@gmail.com>
2014-05-23 07:05:27 +0200

    Arreglado problema con Google Maps KML

--- openerp/addons_extra/google_map_kml/__openerp__.py
+++ openerp/addons_extra/google_map_kml/__openerp__.py
@@ -23,7 +23,7 @@
 ##############################################################################
 {
     'name': 'Google Map KML',
-    'version': '1.1',
+    'version': '1.2',
     'author': 'ErpAndCloud',
     'category': 'Customer Relationship Management',    
     'description': """
