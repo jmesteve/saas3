4c912ca8a27e4d12e40c88069a58396044eb4202
Carlos Escriche Izquierdo <escrichov@gmail.com>
2014-06-10 11:21:03 +0200

    Añadido módulo website_login_colegio para arreglar un problema con su base de datos

--- openerp/addons_extra/website_login_colegio/__init__.py
+++ openerp/addons_extra/website_login_colegio/__init__.py
@@ -4,9 +4,7 @@
 #    OpenERP, Open Source Management Solution
 #    Copyright (c) 2013 jmesteve All Rights Reserved
 #                       https://github.com/jmesteve
-#                       https://github.com/escrichov
 #                       <jmesteve@me.com>
-#                       <escrichov@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published
@@ -22,26 +20,3 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-{
-    'name': "Website Login",
-    'version': '0.35',
-    'category': 'Hidden',
-    'description': """
-        [ENG] Change addon Website Login 
-    """,
-    'author': 'escrichov,jmesteve',
-    'website': 'http://www.erpandcloud.com',
-    'license': 'AGPL-3',
-    "depends": ['base', 'web','website', 'stock', 'auth_signup', 'website_sale', 'payment_paypal', 'payment_ogone', 'payment_adyen', 'product'],
-    'data': ['security/ir.model.access.csv',
-             'views/shop_redirect_view.xml',
-             'views/website_templates.xml',
-             'views/website_sale.xml',
-             'data/website_login_data.xml'
-    ],
-    "application": True,
-    "active": True,
-    "installable": True,
-    'auto_install': False,
-}
